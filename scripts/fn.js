ecbba095eef11ac8fcb09239eabc8f2d00a73cef
AshishKamat21 <103111177+AshishKamat21@users.noreply.github.com>
2023-06-22 19:37:15 +0530

    Add files via upload

---
+++ scripts/fn.js
@@ -0,0 +1,15 @@
+document.DOCUMENT_POSITION_CONTAINS
+
+
+
+const lan=document.getElementById("hin").value;
+
+if(lan==='hin'){
+      document.querySelector('.language-btn')
+      .innerHTML = 'हिंदी';
+}
+else{
+document.querySelector('.language-btn')
+      .innerHTML = 'English';
+
+}
